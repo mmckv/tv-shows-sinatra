166040d7dd3bb608a464b78554ae3e1302298408
Matt McKelvey <mattmckelvey@Matts-MacBook-Pro-2.local>
2016-03-30 16:21:11 -0400

    first commit

---
+++ spec/features/add_tv_show_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+feature "user adds a new TV show" do
+  # As a TV fanatic
+  # I want to add one of my favorite shows
+  # So that I can encourage others to binge watch it
+  #
+  # Acceptance Criteria:
+  # * I must provide the title, network, and starting year.
+  # * I can optionally provide the final year, genre, and synopsis.
+  # * The synopsis can be no longer than 5000 characters.
+  # * The starting year and ending year (if provided) must be
+  #   greater than 1900.
+  # * The genre must be one of the following: Action, Mystery,
+  #   Drama, Comedy, Fantasy
+  # * If any of the above validations fail, the form should be
+  #   re-displayed with the failing validation message.
+
+
+
+  scenario "successfully add a new show" do
+
+end
+
+  scenario "fail to add a show with invalid information" do
+
+
+end
+end
